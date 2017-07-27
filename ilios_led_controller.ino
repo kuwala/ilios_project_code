26a705c57cce1dc8fb1763cf7e29c9ed50c080a9
Daniel Smol <kuwala@gmail.com>
2017-07-27 15:33:33 -0700

    combined sketchs from the led controller and sensor controller into one sketch main sketch.

--- ilios_led_controller.ino
+++ ilios_led_controller.ino
@@ -1,19 +1,23 @@
-/* 
+/*
  *  ilios project led controller
  *  by Jonathon Mooney
  *  code by Jonathon Mooney, Daniel Smolentsev
- *  
+ *
  *  code description:
  *  the led controller receives serial data (from a max patch) in a string format [ r g b r2 g2 b2 r3 g3 b3 ... ]
  *  each set of rgb values is then pushed to the neopixel rings on each side.
- *  
- *  
+ *
+ *
  */
 
 #include <Adafruit_NeoPixel.h>
+
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
+
+#include "ilios_proximity_controller.h"
+
 
 // LED strip pin
 #define PIN          3
@@ -44,33 +48,44 @@ int B4 = 0;
 int R5 = 0;
 int G5 = 0;
 int B5 = 0;
-  
+
 void setup() {
 
   Serial.begin(9600);
 
   // Initialize pixel strips
-//  pixels.setBrightness(64);
+  // pixels.setBrightness(64);
   pixels.begin();
-  
+
    // pixels2.begin();
    Serial.print("ilios_led_controller starting");
+
+
+  // Proximity_controller
+  // Initialize GPIO pins.
+ initPins();
+
+  // Initialize the Wire library.
+  Wire.begin();
 }
 
 void loop() {
 
+  sensorLoop();
+  /*
   readSerial();
   //delay(100);
   drawLEDS(R2,G2,B2);
   //updateLEDS();
+  */
 
 }
 
 void readSerial() {
-  
+
     if(Serial.available()) {
-    
-  
+
+
     R2 = Serial.read();
     G2 = Serial.read();
     B2 = Serial.read();
@@ -86,22 +101,22 @@ void readSerial() {
 //    R5 = Serial.read();
 //    G5 = Serial.read();
 //    B5 = Serial.read();
-  
+
     if(R2 == -1) {
       R2 = 0;
     }
-  
+
     if(B2 == -1) {
       B2 = 0;
     }
-  
+
     if(G2 == -1) {
       G2 = 0;
-     
+
     }
     //Serial.println(String("R2 ") + R2 + String(" G2 ") + G2 + String(" B2 ") + B2);
   }
-  
+
 }
 void updateLEDS() {
   // Timer base draw leds
@@ -115,14 +130,11 @@ void updateLEDS() {
 
 void drawLEDS(int r, int g, int b) {
   for(int i=0;i<NUMPIXELS;i++){
-    pixels.setPixelColor(i, pixels.Color(r, g, b)); 
-    
+    pixels.setPixelColor(i, pixels.Color(r, g, b));
+
   }
-  
+
   pixels.show();
-  
-  
+
+
 }
-
-
-
