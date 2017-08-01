0859474541514a4b6d8a328372f3797b52049d4d
showmethemooney <j.p.mooney@me.com>
2017-07-31 18:57:49 -0700

    Created rgb smoothing first version functions

--- ilios_project_code.ino
+++ ilios_project_code.ino
@@ -1,22 +1,23 @@
 /*
- *  ilios project led controller
- *  by Jonathon Mooney
- *  code by Jonathon Mooney, Daniel Smolentsev
- *
- *  code description:
- *  the led controller receives serial data (from a max patch) in a string format [ r g b r2 g2 b2 r3 g3 b3 ... ]
- *  each set of rgb values is then pushed to the neopixel rings on each side.
- *
- *
- */
+    ilios project led controller
+    by Jonathon Mooney
+    code by Jonathon Mooney, Daniel Smolentsev
+
+    code description:
+    the led controller receives serial data (from a max patch) in a string format [ r g b r2 g2 b2 r3 g3 b3 ... ]
+    each set of rgb values is then pushed to the neopixel rings on each side.
+
+
+*/
 
 #include <Adafruit_NeoPixel.h>
 
 #ifdef __AVR__
-  #include <avr/power.h>
+#include <avr/power.h>
 #endif
 
 #include "ilios_proximity_controller.h"
+#include "rgb_smoothing_variables.h"
 
 
 // LED strip pin
@@ -31,6 +32,9 @@ unsigned long timer2 = 0;
 int timeInterval2 = 40;
 int timeInterval = 20; //the delay time between steps
 int timeStep = 0; // what step are we on 0, 1, 2 ... -> 0
+
+unsigned long pulseTimer = 0;
+int pulseTimeInterval = 40;
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 //Adafruit_NeoPixel pixels2 = Adafruit_NeoPixel(NUMPIXELS, PIN2, NEO_GRB + NEO_KHZ800);
@@ -54,36 +58,45 @@ int R5 = 0;
 int G5 = 0;
 int B5 = 0;
 
+
+
 void setup() {
 
   Serial.begin(230240);
 
   // Initialize pixel strips
   // pixels.setBrightness(64);
- // Serial.print("ilios_led_controller starting");
+  // Serial.print("ilios_led_controller starting");
   pixels.begin();
 
-   // pixels2.begin();
+  // pixels2.begin();
 
 
 
   // Proximity_controller
   // Initialize GPIO pins.
- initPins();
+  initPins();
 
   // Initialize the Wire library.
   Wire.begin();
   initSensors();
+
+  // Init smoothing test
+  for (int i = 0 ; i < NUMREADINGS; i ++ ) {
+    rReadings[i] = 0;
+    gReadings[i] = 0;
+    bReadings[i] = 0;
+  }
 }
 void loop() {
 
-//  Serial.println("sensor loop starting");
-//  sensorLoop();
+  //  Serial.println("sensor loop starting");
+  //  sensorLoop();
   /* // Old LED loop
-  readSerial();
-  //delay(100);
-  drawLEDS(R2,G2,B2);
-  //updateLEDS();
+    readSerial();
+    //delay(100);
+    drawLEDS(R2,G2,B2);
+    //updateLEDS();
   */
 
   //start ranges
@@ -92,47 +105,67 @@ void loop() {
   //delay step
 
   //read readRangeStatus
-    keepReadingSensors();
-   R = sens4;
-   G = sens5;
-   B = sens6;
-   R2 = sens7;
-   G2 = sens8;
-   B2 = sens9;
-   R3 = sens10;
-   G3 = sens11;
-   B3 = sens12;
-R = 255 - R;
-G = 255 - G;
-B = 255 - B;
-R2 = 255 - R2;
-G2 = 255 - G2;
-B2 = 255 - B2;
-R3 = 255 - R3;
-G3 = 255 - G3;
-B3 = 255 - B3;
+  keepReadingSensors();
+  R = sens4;
+  G = sens5;
+  B = sens6;
+  R2 = sens7;
+  G2 = sens8;
+  B2 = sens9;
+  R3 = sens10;
+  G3 = sens11;
+  B3 = sens12;
+  R = 255 - R;
+  G = 255 - G;
+  B = 255 - B;
+  R2 = 255 - R2;
+  G2 = 255 - G2;
+  B2 = 255 - B2;
+  R3 = 255 - R3;
+  G3 = 255 - G3;
+  B3 = 255 - B3;
 
-   printSerial();
-   //drawLEDS(120,30,180);
-   drawLEDS(0, R,G,B);
-   drawLEDS(1, R2,G2,B2);
-   drawLEDS(2, R3,G3,B3);
+
+  printSerial();
+  //drawLEDS(120,30,180);
+  smoothRGBReading();
+  smoothRGB2Reading();
+  smoothRGB3Reading();
+
+  //sudoPulseLEDS();
+  drawLEDS(0, rAverage, gAverage, bAverage);
+  drawLEDS(1, r2Average, g2Average, b2Average);
+  drawLEDS(2, r3Average, g3Average, b3Average);
 
 
 
 }
+
+void sudoPulseLEDS() {
+  if (millis() - pulseTimer > pulseTimeInterval) {
+    gTotal += 1000;
+    rTotal += 1000;
+    bTotal += 1000;
+    pulseTimer = millis();
+  }
+
+}
+
+
+
+
 void printSerial() {
 
   if (millis() - timer2 > timeInterval2) {
     timer2 = millis();
-    
+
     Serial.println(sens1 + String(" ") + sens2 + String(" ") + sens3 + String(" ") + sens4 + String(" ") + sens5 + String(" ") + sens6 + String(" ") + sens7 + String(" ") + sens8 + String(" ") + sens9 + String(" ") + sens10 + String(" ") + sens11 + String(" ") + sens12);
   }
 }
 
 void readSerial() {
 
-    if(Serial.available()) {
+  if (Serial.available()) {
 
     //Serial.println(String("R2 ") + R2 + String(" G2 ") + G2 + String(" B2 ") + B2);
   }
@@ -145,7 +178,7 @@ void updateLEDS() {
     //randomLEDS();
     timer = millis();
   }
- // Serial.println("updating leds");
+  // Serial.println("updating leds");
 }
 
 void readRanges() {
@@ -162,12 +195,12 @@ void readRanges() {
   sens10 = readRangeResult(Add_22);
   sens11 = readRangeResult(Add_23);
   sens12 = readRangeResult(Add_24);
-//    sens13 = readRangeResult(Add_10);
-//    sens14 = readRangeResult(Add_11);
-//    sens15 = readRangeResult(Add_12);
-//    sens16 = readRangeResult(Add_19);
-//    sens17 = readRangeResult(Add_20);
-//    sens18 = readRangeResult(Add_21);
+  //    sens13 = readRangeResult(Add_10);
+  //    sens14 = readRangeResult(Add_11);
+  //    sens15 = readRangeResult(Add_12);
+  //    sens16 = readRangeResult(Add_19);
+  //    sens17 = readRangeResult(Add_20);
+  //    sens18 = readRangeResult(Add_21);
 }
 void startRanges() {
   startRange(Add_1);
@@ -183,12 +216,12 @@ void startRanges() {
   startRange(Add_22);
   startRange(Add_23);
   startRange(Add_24);
-//    startRange(Add_10);
-//    startRange(Add_11);
-//    startRange(Add_12);
-//    startRange(Add_19);
-//    startRange(Add_20);
-//    startRange(Add_21);
+  //    startRange(Add_10);
+  //    startRange(Add_11);
+  //    startRange(Add_12);
+  //    startRange(Add_19);
+  //    startRange(Add_20);
+  //    startRange(Add_21);
 }
 
 void keepReadingSensors() {
@@ -200,26 +233,26 @@ void keepReadingSensors() {
     timeStep += 1;
 
     if (timeStep == 1) {
-    //
-        startRanges();
-      } else if (timeStep >= 2) {
-        //
-        readRanges();
-        timeStep = 0;
-      }
-      
+      //
+      startRanges();
+    } else if (timeStep >= 2) {
+      //
+      readRanges();
+      timeStep = 0;
+    }
+
   }
 
   //
-  
+
 }
 
 void drawLEDS(int set, int r, int g, int b) {
   int pixelSetLength = 12;
   int start = pixelSetLength * set;
   int end = start + pixelSetLength;
-  
-  for(int i=start;i<end;i++){
+
+  for (int i = start; i < end; i++) {
     pixels.setPixelColor(i, pixels.Color(r, g, b));
 
   }
@@ -228,3 +261,104 @@ void drawLEDS(int set, int r, int g, int b) {
 
 
 }
+
+
+
+// RGB smoothing functions quick test
+// Needs to be optimized 
+
+void smoothRGBReading() {
+  // raw values
+
+  gTotal = gTotal - gReadings[gReadIndex];
+  gReadings[gReadIndex] = G;
+  gTotal = gTotal + gReadings[gReadIndex];
+  gReadIndex ++;
+  if (gReadIndex >= NUMREADINGS) {
+    gReadIndex = 0;
+  }
+  gAverage = gTotal / NUMREADINGS;
+
+  rTotal = rTotal - rReadings[rReadIndex];
+  rReadings[rReadIndex] = R;
+  rTotal = rTotal + rReadings[rReadIndex];
+  rReadIndex ++;
+  if (rReadIndex >= NUMREADINGS) {
+    rReadIndex = 0;
+  }
+  rAverage = rTotal / NUMREADINGS;
+
+  bTotal = bTotal - bReadings[bReadIndex];
+  bReadings[bReadIndex] = B;
+  bTotal = bTotal + bReadings[bReadIndex];
+  bReadIndex ++;
+  if (bReadIndex >= NUMREADINGS) {
+    bReadIndex = 0;
+  }
+  bAverage = bTotal / NUMREADINGS;
+}
+
+void smoothRGB2Reading() {
+  // raw values
+
+  g2Total = g2Total - g2Readings[g2ReadIndex];
+  g2Readings[g2ReadIndex] = G2;
+  g2Total = g2Total + g2Readings[g2ReadIndex];
+  g2ReadIndex ++;
+  if (g2ReadIndex >= NUMREADINGS) {
+    g2ReadIndex = 0;
+  }
+  g2Average = g2Total / NUMREADINGS;
+
+  r2Total = r2Total - r2Readings[r2ReadIndex];
+  r2Readings[r2ReadIndex] = R2;
+  r2Total = r2Total + r2Readings[r2ReadIndex];
+  r2ReadIndex ++;
+  if (r2ReadIndex >= NUMREADINGS) {
+    r2ReadIndex = 0;
+  }
+  r2Average = r2Total / NUMREADINGS;
+
+  b2Total = b2Total - b2Readings[b2ReadIndex];
+  b2Readings[b2ReadIndex] = B2;
+  b2Total = b2Total + b2Readings[b2ReadIndex];
+  b2ReadIndex ++;
+  if (b2ReadIndex >= NUMREADINGS) {
+    b2ReadIndex = 0;
+  }
+  b2Average = b2Total / NUMREADINGS;
+}
+
+void smoothRGB3Reading() {
+  // raw values
+
+  g3Total = g3Total - g3Readings[g3ReadIndex];
+  g3Readings[g3ReadIndex] = G3;
+  g3Total = g3Total + g3Readings[g3ReadIndex];
+  g3ReadIndex ++;
+  if (g3ReadIndex >= NUMREADINGS) {
+    g3ReadIndex = 0;
+  }
+  g3Average = g3Total / NUMREADINGS;
+
+  r3Total = r3Total - r3Readings[r3ReadIndex];
+  r3Readings[r3ReadIndex] = R3;
+  r3Total = r3Total + r3Readings[r3ReadIndex];
+  r3ReadIndex ++;
+  if (r3ReadIndex >= NUMREADINGS) {
+    r3ReadIndex = 0;
+  }
+  r3Average = r3Total / NUMREADINGS;
+
+  b3Total = b3Total - b3Readings[b3ReadIndex];
+  b3Readings[b3ReadIndex] = B3;
+  b3Total = b3Total + b3Readings[b3ReadIndex];
+  b3ReadIndex ++;
+  if (b3ReadIndex >= NUMREADINGS) {
+    b3ReadIndex = 0;
+  }
+  b3Average = b3Total / NUMREADINGS;
+}
+
+
+
---
+++ rgb_smoothing_variables.h
@@ -0,0 +1,56 @@
+//Smoothing Test
+
+// Samples read for the average function
+#define NUMREADINGS 50
+
+// Face 1
+int gReadings[NUMREADINGS];
+int gTotal = 0;
+int gReadIndex = 0;
+int gAverage = 0;
+
+int bReadings[NUMREADINGS];
+int bTotal = 0;
+int bReadIndex = 0;
+int bAverage = 0;
+
+int rReadings[NUMREADINGS];
+int rTotal = 0;
+int rReadIndex = 0;
+int rAverage = 0;
+
+// Face 2
+
+int g2Readings[NUMREADINGS];
+int g2Total = 0;
+int g2ReadIndex = 0;
+int g2Average = 0;
+
+int b2Readings[NUMREADINGS];
+int b2Total = 0;
+int b2ReadIndex = 0;
+int b2Average = 0;
+
+int r2Readings[NUMREADINGS];
+int r2Total = 0;
+int r2ReadIndex = 0;
+int r2Average = 0;
+
+// Face 3
+
+int g3Readings[NUMREADINGS];
+int g3Total = 0;
+int g3ReadIndex = 0;
+int g3Average = 0;
+
+int b3Readings[NUMREADINGS];
+int b3Total = 0;
+int b3ReadIndex = 0;
+int b3Average = 0;
+
+int r3Readings[NUMREADINGS];
+int r3Total = 0;
+int r3ReadIndex = 0;
+int r3Average = 0;
+
+
